b14c527c537f2c9c875c50d146687b14784d47de
jesusarockias@syncfusion.com <jesusarockias@syncfusion.com>
2021-01-04 16:57:57 +0530

    committing sample

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ej2-js-webpack
-TypeScript sample creation from scratch with Syncfusion EJ2 Components and webpack configurations
+JavaScript ES6 sample creation from scratch with Syncfusion EJ2 Components and webpack configurations.
+
+## Installation
+
+Run ``npm install`` to install the required packages.
+
+## Run the sample
+
+Run ``npm run start`` command to run the sample.
---
+++ index.js
@@ -0,0 +1,23 @@
+import { Grid, Group, Filter, Page, Sort } from '@syncfusion/ej2-grids';
+import { data } from './datasource.js';
+
+Grid.Inject(Group, Filter, Page, Sort);
+
+import './style.css';
+
+let grid = new Grid({
+    dataSource: data,
+    columns: [
+                { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 120, type: 'number' },
+                { field: 'CustomerID', width: 140, headerText: 'Customer ID', type: 'string' },
+                { field: 'Freight', headerText: 'Freight', textAlign: 'Right', width: 120, format: 'C' },
+                { field: 'OrderDate', headerText: 'Order Date', width: 140, format: 'yMd' }
+    ],
+    height: 175,
+    allowGrouping: true,
+    allowPaging: true,
+    allowSorting: true,
+    allowFiltering: true
+});
+
+grid.appendTo('#Grid');
